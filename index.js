422f1ed4d5c6b5684c379b44288ff856763d26be
benjaminbloch <153328914+benjaminbloch@users.noreply.github.com>
2024-05-16 21:24:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,30 @@ const client = new Discord.Client();
 let interval;
 client.on('message', async msg => {
   switch (msg.content) {
-    case "ping":
-      msg.reply("Pong!");
+  case "start":
+      msg.reply("Hello! I am going to help you today. I will try my best to support you with what you need.);
+      msg.reply("/lock");
+      break;
+  case "claim":
+      msg.reply("Ok! I cant do that. Please wait for the owner/staff to support you.");
+      msg.reply("/lock");
+      break;
+ case "purchase":
+      msg.reply("Ok! I will help you with this. Please specify the item you would like to buy. If you dont knoow, please say ``idk``");
+      break;
+ case "idk":
+      msg.reply("```t0kens``` ```nitro (1m)``` ```nitro (3m)``` ```nitro 1m giftlink``` ```nitro 1 year giftlink`` **NOTE: please write the exact command**");
+      break;
+case "claim roles":
+      msg.reply("Ok! I cant do that. Please wait for the owner/staff to support you.");
+      msg.reply("/lock");
       break;
     case "!meme":
       msg.channel.send("Here's your meme!");
       const img = await getMeme();
       msg.channel.send(img);
       break;
-    case "!eye":
-      msg.channel.send("You are now subscribed to eye reminders.");
-       interval = setInterval (function () {
-        msg.channel.send("Please take an eye break now!")
-        .catch(console.error); 
-      }, 3600000); //every hour
-      break;
-    case "!stop":
-      msg.channel.send("I have stopped eye reminders.");
-      clearInterval(interval);
-      break;
+   
   }
 });
 
